ce5fd683672430691263b039f9b1572a100b318d
hublaw21 <39032867+hublaw21@users.noreply.github.com>
2018-05-15 13:22:38 -0400

    Fifth attempt

--- app/src/main/java/com/skaterbayusa/test/mytestappv2a/MainActivity.java
+++ app/src/main/java/com/skaterbayusa/test/mytestappv2a/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private ElementsAdapter mAdapter;
     //String elementIDList[] = {"1T", "1S", "1F", "1Lo", "1Lz", "1A"};
     //String elementNameList[] = {"Single Toe", "Single Salchow", "Single Flip", "Single Loop", "Single Lutz", "Single Axel"};
-    String elementList[][] = {{"1T","Single Toe"},{"1S","Single Salchow"},{"1F","Single Flip"},{"1Lo","Single Loop"},{"1Lz", "Single Lutz"},{"1A","Single Axel"}};
+    //String elementList[][] = {{"1T","Single Toe"},{"1S","Single Salchow"},{"1F","Single Flip"},{"1Lo","Single Loop"},{"1Lz", "Single Lutz"},{"1A","Single Axel"}};
 
 
     @Override
@@ -33,11 +33,10 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.elements_list);
         ArrayList<Elements> elementsList = new ArrayList<>();
 
-        /*
         elementsList.add(new Elements("1T","Single Toe"));
         elementsList.add(new Elements("1S","Single Salchow"));
         elementsList.add(new Elements("1F","Single Flip"));
-        */
+
         mAdapter = new ElementsAdapter(this,elementsList);
         listView.setAdapter(mAdapter);
 
